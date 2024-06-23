c5de36029cd2474aa5efd25fbbd15f9bee4c772b
B-Phan <brandon.phan2@alumni.ucalgary.ca>
2024-06-23 18:33:46 -0400

    linting models 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,10 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# Create a Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -15,7 +14,7 @@ class CarMake(models.Model):
         return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# Create a Car Model model
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -35,4 +34,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
